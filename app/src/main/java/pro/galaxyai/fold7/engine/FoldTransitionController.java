cefd8525a3b723391ad6dc65492e58cdc3b70472
ControlCenterSoft <40738562+ControlCenterSoft@users.noreply.github.com>
2026-09-23 00:47:50 +0300

    Add Fold Transition Controller v4

---
+++ app/src/main/java/pro/galaxyai/fold7/engine/FoldTransitionController.java
@@ -0,0 +1,15 @@
+package pro.galaxyai.fold7.engine;
+
+public class FoldTransitionController {
+    private float progress;
+
+    public void update(boolean opened){
+        progress += opened ? 0.02f : -0.02f;
+        if(progress<0) progress=0;
+        if(progress>1) progress=1;
+    }
+
+    public float getProgress(){
+        return progress;
+    }
+}
