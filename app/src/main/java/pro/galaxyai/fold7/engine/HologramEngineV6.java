b03b17c3c096b89cf2c692c4b5136732839edc1a
ControlCenterSoft <40738562+ControlCenterSoft@users.noreply.github.com>
2026-09-23 05:08:27 +0300

    v6: add hologram engine

---
+++ app/src/main/java/pro/galaxyai/fold7/engine/HologramEngineV6.java
@@ -0,0 +1,13 @@
+package pro.galaxyai.fold7.engine;
+
+public class HologramEngineV6 {
+    private float intensity = 1.0f;
+
+    public void setIntensity(float value){
+        intensity = value;
+    }
+
+    public float getIntensity(){
+        return intensity;
+    }
+}
